094749e5ba45d2921e9ab04281c6ef985847c3fa
Orkhan <baku.vip01@gmail.com>
2025-03-21 12:50:57 +0400

    first commit

---
+++ src/main/java/com/test/dream_shop/model/Category.java
@@ -0,0 +1,32 @@
+package com.test.dream_shop.model;
+
+import java.util.List;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private List<Product> products;
+
+}
---
+++ src/main/java/com/test/dream_shop/model/Product.java
@@ -0,0 +1,47 @@
+package com.test.dream_shop.model;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+
+    private String brand;
+
+    private String description;
+
+    private Integer inventory;
+
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+
+    @JoinColumn(name = "category_id")
+    @ManyToOne
+    private Category category;  
+}
